f4bb8af8e5c4374ca0e2af06b0c9b609ed808529
Alpha Olomi <alphaolomi@gmail.com>
2020-04-15 23:19:48 +0300

    wip: rewrite in app models

--- app/views.py
+++ app/views.py
@@ -1,22 +1,12 @@
+# Flask modules
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-#from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-app = Flask(__name__)
-
-# Config MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
-app.config['MYSQL_DB'] = 'myflaskapp'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# init MYSQL
-mysql = MySQL(app)
-
-#Articles = Articles()
+# App modules
+from app  import app
 
 # Index
 @app.route('/')
@@ -275,7 +265,3 @@ def delete_article(id):
     flash('Article Deleted', 'success')
 
     return redirect(url_for('dashboard'))
-
-if __name__ == '__main__':
-    app.secret_key='secret123'
-    app.run(debug=True)
